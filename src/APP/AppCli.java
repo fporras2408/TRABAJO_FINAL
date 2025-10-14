717ed66d60de789f2699c4aacf25d0a2e44b59d7
Francisco Porras <franciscoporras2408@gmail.com>
2025-10-14 19:58:13 -0300

    este es el nuevo cambio

--- src/APP/AppCli.java
+++ src/APP/AppCli.java
@@ -1,6 +1,6 @@
 package APP;
 public class AppCli {
     public static void main(String[] args) throws Exception {
-        System.out.println("hola gay");
+        System.out.println("hola soy gay");
     }
 }
