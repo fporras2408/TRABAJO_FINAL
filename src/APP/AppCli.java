03196528e8752a2e9d0df37d83925b394eadf8da
franciscoporras2408@gmail.com <franciscoporras2408@gmail.com>
2025-10-14 19:48:51 -0300

    primer commit de fran el palido

--- src/APP/AppCli.java
+++ src/APP/AppCli.java
@@ -1,6 +1,6 @@
 package APP;
 public class AppCli {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("hola mundo");
     }
 }
